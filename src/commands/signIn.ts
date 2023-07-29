fa611452c62819b29cd5a2dafa0c5bafd05e404c
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-07-29 07:55:26 +0300

    feat: reply password

--- src/commands/signIn.ts
+++ src/commands/signIn.ts
@@ -2,7 +2,11 @@ import { Context } from 'telegraf';
 import { findOne } from '../mongo/findOne';
 
 const signIn = () => async (ctx: Context) => {
-  await ctx.reply('Enter password:');
+  await ctx.reply('Enter password:', {
+    reply_markup: {
+      force_reply: true,
+    },
+  });
   findOne(ctx);
 };
 
