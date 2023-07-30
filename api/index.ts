40e781c7500fd3c1dfd0687aa48a221eaa41cb02
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-07-30 15:47:42 +0300

    fix: catch error 6

--- api/index.ts
+++ api/index.ts
@@ -5,9 +5,11 @@ export default async function handle(req: VercelRequest, res: VercelResponse) {
   try {
     await startVercel(req, res);
   } catch (e: any) {
+    console.log('error in startVercel1');
     res.statusCode = 500;
     // res.setHeader('Content-Type', 'text/html');
     // res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
     console.error(e.message);
+    console.log('error in startVercel2');
   }
 }
--- src/core/production.ts
+++ src/core/production.ts
@@ -21,7 +21,7 @@ const production = async (
 
   if (req.method === 'POST') {
     await bot.handleUpdate(req.body as unknown as Update, res);
-    res.status(201).json(req.body);
+    res.status(200).json(req.body);
   } else {
     res.status(200).json('Listening to bot events...');
   }
