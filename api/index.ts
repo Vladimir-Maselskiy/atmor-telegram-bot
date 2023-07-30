aa24cad07483490f0c05c7c53fff73ef60ff1bbe
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-07-30 15:54:30 +0300

    fix: catch error 8

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,7 @@ export default async function handle(req: VercelRequest, res: VercelResponse) {
     res.statusCode = 500;
     // res.setHeader('Content-Type', 'text/html');
     // res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
-    console.error(e.message);
-    console.log('error in startVercel2');
+    // console.error(e.message);
+    // console.log('error in startVercel2');
   }
 }
