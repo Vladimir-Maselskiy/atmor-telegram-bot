358121ab0df8391a1cc157be833741a423e93b0a
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-07-30 15:12:00 +0300

    fix: catch error 1

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ export default async function handle(req: VercelRequest, res: VercelResponse) {
   try {
     await startVercel(req, res);
   } catch (e: any) {
-    res.statusCode = 500;
+    res.statusCode = 510;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
     console.error(e.message);
